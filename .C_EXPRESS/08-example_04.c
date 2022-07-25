9f489cde4e0406e3a5453169d8da3326bdc84da1
leaneroov <leaneroov@gmail.com>
2022-07-25 22:45:48 +0900

    practice

---
+++ .C_EXPRESS/08-example_04.c
@@ -0,0 +1,13 @@
+//88p 04번 달력 출력 
+#include <stdio.h>
+int main(void)
+{
+    printf("S    M   T   W   T   F   S\n");
+    printf("1    2   3   4   5   6   7\n");
+    printf("8    9   10  11  12  13  14\n"); 
+    printf("15   16  17  18  19  20  21\n");
+    printf("22   23  24  25  26  27  28\n");
+    printf("29   30  31\n");
+    return 0;
+}
+
