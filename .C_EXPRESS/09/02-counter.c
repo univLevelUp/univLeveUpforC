af8e433d5db8954b6a814e6062aa6866d511a04c
leaneroov <leaneroov@gmail.com>
2022-08-28 20:27:38 +0900

    practice

---
+++ .C_EXPRESS/09/02-counter.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void inc(int counter);
+
+int main(void)
+{
+    int i = 10;
+
+    printf("berfor: i=%d\n", i);
+    inc(i);
+    printf("after: i=%d\n", i);
+
+    return 0;
+}
+
+void inc(int counter)
+{
+    counter
+
+    ;
+}
---
+++ .C_EXPRESS/09/04-counter2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int counter;
+
+int main(void)
+{
+    printf("counter=%d", counter);
+    return 0;
+}
