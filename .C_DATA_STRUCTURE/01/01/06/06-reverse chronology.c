862cbd933cc5f71e2ecf58e322bda258e0cc3ecc
leaneroov <leaneroov@gmail.com>
2022-11-17 22:48:26 +0900

    practice

---
+++ .C_DATA_STRUCTURE/01/01/06/06-reverse chronology.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+
+typedef struct ListNode
+{
+    element data;
+    struct ListNode *link;
+}ListNode;
+
+ListNode* insert_frist(ListNode *head, element value)
+{
+    ListNode *p = (ListNode *)malloc(sizeof(ListNode));
+    p->data = value;
+    p->link = head;
+    head = p;
+    return head;
+}
+
+void print_list(ListNode *head)
+{
+    for(ListNode *p = head; p != NULL; p = p->link)
+        print("%d->", p->data);
+    printf("NULL \n");
+}
+
+ListNode* reverse(ListNode *head)
+{
+    ListNode *p, *q, *r;
+
+    p = head;
+    q = NULL;
+    while(p != NULL)
+    {
+        r = q;
+
+        q = p;
+
+        p = p->link;
+        q->link = r;
+    }
+    return q;
+}
+
+int main(void)
+{
+    ListNode* head1 = NULL;
+    ListNode* head2 = NULL;
+
+    head1 = insert_frist(head1, 10);
+    head1 = insert_frist(head1, 20);
+    head1 = insert_frist(head1, 30);
+    print_list(head1);
+
+    head2 = reverse(head1);
+    print_list(head2);
+
+    return 0;
+}
