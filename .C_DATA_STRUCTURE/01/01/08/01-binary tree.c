23856eb4b5bbcbdc153b4c4e1a18a1fc430a9a10
leaneroov <leaneroov@gmail.com>
2022-11-24 23:55:31 +0900

    practice

--- .C_DATA_STRUCTURE/01/01/08/01-binary tree.c
+++ .C_DATA_STRUCTURE/01/01/08/01-binary tree.c
@@ -13,13 +13,13 @@ typedef struct TreeNode{
 int main(void)
 {
     TreeNode *n1, *n2, *n3;
-    n1 = (TreeNode *)malloc(sizeof(TreeNode);
-    n2 = (TreeNode *)malloc(sizeof(TreeNode);
-    n3 = (TreeNode *)malloc(sizeof(TreeNode);
+    n1 = (TreeNode *)malloc(sizeof(TreeNode));
+    n2 = (TreeNode *)malloc(sizeof(TreeNode));
+    n3 = (TreeNode *)malloc(sizeof(TreeNode));
     n1->data = 10;
     n2->left = n2;
     n1->right = n3;
-    ne->data = 20;
+    n2->data = 20;
     n2->left = NULL;
     n2->right = NULL;
     n3->data = 30;
