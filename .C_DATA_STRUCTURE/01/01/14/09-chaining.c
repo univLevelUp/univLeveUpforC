3bf4785b093867fb17e2c3abadddd9890fc720eb
leaneroov <leaneroov@gmail.com>
2023-01-05 21:32:21 +0900

    practice

---
+++ .C_DATA_STRUCTURE/01/01/14/09-chaining.c
@@ -0,0 +1,38 @@
+int hash_function(int key)
+{
+    return key % TABLE_SIZE;
+}
+
+void hash_chain_add(element item, struct list *ht[])
+{
+    int hash_value = hash_function(item.key);
+    struct list *ptr;
+    struct list *node_before = NULL, *node = ht[hash_value];
+    for(; node; node_before = node; node = node->link){
+        if(node->item.key == item.key){
+            fprintf(stderr, "Navigation keys already saved\n");
+            return;
+        }
+    }
+    ptr = (struct list *)malloc(sizeof(sturct list))
+    ptr->item = item;
+    ptr->link = NULL;
+    if(node_before)
+        node_before->link = ptr;
+    else
+        ht[hash_value] = ptr;
+}
+
+void hash_chain_search(element item, struct list *ht[])
+{
+    struct list *node;
+
+    int hash_vaule = hash_function(item.key);
+    for(node = ht[hash_vaule]; node; node = node->link){
+        if(node->item.key == iten.key){
+            fprintf(stderr, "Navigation %d Successful\n", item.key)
+            return;
+        }
+    }
+    printf("Key not found");
+}
