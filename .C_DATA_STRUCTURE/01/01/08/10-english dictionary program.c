6fccd1d3476da72eb8df441c8eb4d21bdbd4924f
leaneroov <leaneroov@gmail.com>
2023-01-19 23:36:43 +0900

    practice

--- .C_DATA_STRUCTURE/01/01/08/10-english dictionary program.c
+++ .C_DATA_STRUCTURE/01/01/08/10-english dictionary program.c
@@ -1,59 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <memory.h>
-
-#define MAX_WORD_SIZE 100
-#define MAX_MEANING_SIZE 200
-
-typedef struct {
-    char word[MAX_WORD_SIZE];
-    char meaning[MAX_MEANING_SIZE];
-} element;
-
-typedef struct TreeNode{
-    element key;
-    struct TreeNode *left, *right;
-} TreeNode;
-
-int compare(element e1, element e2)
-{
-    return strcmp(e1.word, e2.word);
-}
-
-void display(TreeNode *p)
-{
-    if(p != NULL){
-        printf("(");
-        display(p->left);
-        printf("%s:%s", p->key.word, p->key.meaning);
-        display(p->right);
-        printf(")");
-    }
-}
-
-TreeNode * search(TreeNode * root, element key)
-{
-    TreeNode *p = root;
-    while(p != NULL){
-        if(compare(key, p->key) == 0)
-            return p;
-        else if(compare(key, p->key) < 0)
-            p = p->left;
-        else if(compare(key, p->key) > 0)
-            p = p->right;
-    }
-    return p;
-}
-
-TreeNode * new_node(element item)
-{
-    TreeNode * temp = (TreeNode *)malloc(sizeof(TreeNode));
-    temp->key = item;
-    temp->left = temp->right = NULL;
-    return temp;
-}
-
 TreeNode * insert_node(TreeNode * node, element key)
 {
     if(node == NULL) return new_node(key);
@@ -64,16 +8,6 @@ TreeNode * insert_node(TreeNode * node, element key)
         node->right = insert_node(node->right, key);
 
     return node;
-}
-
-TreeNode * min_value_node(TreeNode * node)
-{
-    TreeNode * current = node;
-    while (current->left != NULL)
-    {
-        current = current -> left;
-    }
-    return current;
 }
 
 TreeNode * delete_node(TreeNode *root, element key){
@@ -100,50 +34,4 @@ TreeNode * delete_node(TreeNode *root, element key){
         root->right = delete_node(root->right, temp->key);
     }
     return root;
-}
-
-void help()
-{
-    printf("\n*** i: input , d: delete, s:search, p: print, q: termination ****: ");
-}
-
-int main(void)
-{
-    char command;
-    element e;
-    TreeNode * root = NULL;
-    TreeNode * tmp;
-
-    do{
-        help();
-        command = getchar();
-        getchar();
-        switch(command){
-            case 'i':
-                printf("word:");
-                gets_s(e.word, MAX_WORD_SIZE);
-                printf("meaning:");
-                gets_s(e.meaning, MAX_MEANING_SIZE);
-                root = insert_node(root, e);
-                break;
-            case 'd':
-                printf("word:");
-                get_s(e.word, MAX_WORD_SIZE);
-                root = delete_node(root, e);
-                break;
-            case 'p':
-                display(root);
-                printf("\n");
-                break;
-            case 's':
-                printf("word:");
-                gets_s(e.word, MAX_WORD_SIZE);
-                tmp = search(root , e);
-                if(tmp != NULL)
-                    printf("meaning: %s\n", e.meaning);
-                break;
-        }
-    }while(command != 'q');
-    
-    return 0;
 }
