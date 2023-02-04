52cd870bcb8efff5d629e600e4a970a97e859e87
leaneroov <leaneroov@gmail.com>
2023-02-04 22:11:09 +0900

    practice

---
+++ A hot-blooded c programming language/Chapter 05/07-ploblem03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int num;
+  printf("정수의 값: ");
+  scanf("%d", &num);
+
+  printf("%c", num);
+}
---
+++ A hot-blooded c programming language/Chapter 05/08-ploblem04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char ch;
+  printf("아스키 문자: ");
+  scanf("%c", &ch);
+  printf("아스키 코드 값: %d \n", ch);
+  return 0;
+}
