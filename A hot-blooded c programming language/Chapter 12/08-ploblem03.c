5adcf1972bbddb355375e885eb662a91093002c0
leaneroov <leaneroov@gmail.com>
2023-02-25 17:36:31 +0900

    practice

---
+++ A hot-blooded c programming language/Chapter 12/08-ploblem03.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int arr[5] = {1, 2, 3, 4 ,5};
+  int *ptr = arr;
+
+  for(int i = 0; i < 5; i++)
+  {
+    *ptr += 2;
+    ptr++;
+  }
+
+  for(int i = 0; i < 5; i++)
+    printf("%d ", arr[i]);
+  return 0;
+}
