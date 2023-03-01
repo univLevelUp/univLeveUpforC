2de0ffc7603a3c799fe076991c4b095c147bfc58
leaneroov <leaneroov@gmail.com>
2023-03-01 20:26:41 +0900

    practice

---
+++ A hot-blooded c programming language/Chapter 13/05-ploblem01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int SquareByValue(int num)
+{
+    return num * num;
+}
+
+void SquareByReference(int * ptr)
+{
+    int num = *ptr;
+    *ptr = num * num;
+}
+
+int main(void)
+{
+    int num = 10;
+    printf("%d \n", SquareByValue(num));
+    SquareByReference(&num);
+    printf("%d \n", num);
+    return 0;
+}
